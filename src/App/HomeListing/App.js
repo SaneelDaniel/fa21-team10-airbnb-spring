2df981e441fcd4766d0c607f3c5bec0077b67033
methawadee <methawadee@Mayyers-macbook.local>
2021-12-04 15:01:42 -0800

    frontend completed

---
+++ src/App/HomeListing/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Footer from './Footer';
+import Home from './Home';
+import SearchPage from './SearchPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
