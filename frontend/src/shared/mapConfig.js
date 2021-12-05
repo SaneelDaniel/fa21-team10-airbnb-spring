90ebe0843af094ec39314c9a16ecb39a44c68606
Varun Reddy <methawadee@Mayyers-macbook.local>
2021-12-04 18:00:47 -0800

    frontend changes

--- frontend/src/shared/mapConfig.js
+++
@@ -1,4 +0,0 @@
-export const bootstrapURLKeys = {
-  key: 'AIzaSyBUvk0z5VicbqlUh-7cccmJypbk72uxNQU',
-  v: '3.36',
-};
--- frontend/src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import RootReducer from './reducers/RootReducer';
-
-const configureStore = (preloadedState = {}) =>
-  createStore(RootReducer, preloadedState, applyMiddleware(logger, thunk));
-
-export default configureStore;
